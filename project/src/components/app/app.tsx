868cd973b2daffc71eaa9ebbb71e7bb8f524aa85
Ansagan Islamgali <islamgaliev@mail.ru>
2021-10-09 22:40:00 +0500

    TASK#4: Creates Routes for all the links of project

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Main from '../main/main';
 import { Film } from '../../types/film';
+import SignIn from '../sign-in/sign-in';
+import MyList from '../my-list/my-list';
+import FilmPage from '../film-page/film-page';
+import AddReview from '../add-review/add-review';
+import Player from '../player/player';
+import NotFound from '../not-found/not-found';
+import { AppRoute } from '../../constants';
 
 type AppProps = {
   films: Film[],
@@ -8,10 +16,31 @@ type AppProps = {
 
 function App({ films, promoFilm }: AppProps): JSX.Element {
   return (
-    <Main
-      films={films}
-      promoFilm={promoFilm}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route path={AppRoute.Main} exact>
+          <Main films={films} promoFilm={promoFilm}/>
+        </Route>
+        <Route path={AppRoute.SignIn} exact>
+          <SignIn/>
+        </Route>
+        <Route path={AppRoute.MyList} exact>
+          <MyList myList={films}/>
+        </Route>
+        <Route path={AppRoute.FilmPage} exact>
+          <FilmPage film={promoFilm} relatedFilms={films.slice(0, 4)}/>
+        </Route>
+        <Route path={AppRoute.AddReview} exact>
+          <AddReview film={promoFilm}/>
+        </Route>
+        <Route path={AppRoute.Player} exact>
+          <Player film={promoFilm}/>
+        </Route>
+        <Route>
+          <NotFound/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,3 +1,4 @@
+import './css/main.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
