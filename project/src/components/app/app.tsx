364e21b602bcd4e3d17be3e797709eaa674d703b
Ansagan Islamgali <islamgaliev@mail.ru>
2021-10-09 22:45:37 +0500

    TASK#4: Creates PrivateRotes for /mylist and /films/:id/review

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Main from '../main/main';
 import { Film } from '../../types/film';
 import SignIn from '../sign-in/sign-in';
@@ -7,7 +7,8 @@ import FilmPage from '../film-page/film-page';
 import AddReview from '../add-review/add-review';
 import Player from '../player/player';
 import NotFound from '../not-found/not-found';
-import { AppRoute } from '../../constants';
+import { AppRoute, AuthorizationStatus } from '../../constants';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   films: Film[],
@@ -24,15 +25,21 @@ function App({ films, promoFilm }: AppProps): JSX.Element {
         <Route path={AppRoute.SignIn} exact>
           <SignIn/>
         </Route>
-        <Route path={AppRoute.MyList} exact>
-          <MyList myList={films}/>
-        </Route>
+        <PrivateRoute
+          exact
+          path={AppRoute.MyList}
+          render={() => <MyList myList={films}/>}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        />
         <Route path={AppRoute.FilmPage} exact>
           <FilmPage film={promoFilm} relatedFilms={films.slice(0, 4)}/>
         </Route>
-        <Route path={AppRoute.AddReview} exact>
-          <AddReview film={promoFilm}/>
-        </Route>
+        <PrivateRoute
+          exact
+          path={AppRoute.AddReview}
+          render={() => <AddReview film={promoFilm}/>}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        />
         <Route path={AppRoute.Player} exact>
           <Player film={promoFilm}/>
         </Route>
