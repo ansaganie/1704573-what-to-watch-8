8793baacc2472f8582bbfaa685f03cb8024dd785
ansagan <islamgaliev@mail.ru>
2021-10-17 00:35:30 +0500

    TASK#7: Creates films tab component and includes into film page

--- project/src/components/film-page/film-page.tsx
+++ project/src/components/film-page/film-page.tsx
@@ -7,6 +7,7 @@ import FilmsList from '../films-list/films-list';
 import Sprite from '../sprite/sprite';
 import { films } from '../../mock/films';
 import { scrollToFilmTitle } from '../../utils/side-effects';
+import FilmTabs from '../film-tabs/film-tabs';
 
 type MoviePageProps = {
   relatedFilms?: Film[],
@@ -71,39 +72,7 @@ function FilmPage(props: MoviePageProps): JSX.Element {
             </div>
 
             <div className="film-card__desc">
-              <nav className="film-nav film-card__nav">
-                <ul className="film-nav__list">
-                  <li className="film-nav__item film-nav__item--active">
-                    <a href="#" className="film-nav__link">Overview</a>
-                  </li>
-                  <li className="film-nav__item">
-                    <a href="#" className="film-nav__link">Details</a>
-                  </li>
-                  <li className="film-nav__item">
-                    <a href="#" className="film-nav__link">Reviews</a>
-                  </li>
-                </ul>
-              </nav>
-
-              <div className="film-rating">
-                <div className="film-rating__score">{film.rating}</div>
-                <p className="film-rating__meta">
-                  <span className="film-rating__level">Very good</span>
-                  <span className="film-rating__count">{film.scoresCount} ratings</span>
-                </p>
-              </div>
-
-              <div className="film-card__text">
-
-                {film.description.map((desc) => <p key={desc.slice(0, 6)}>{desc}</p>)}
-
-                <p className="film-card__director"><strong>Director: {film.director}</strong></p>
-                <p className="film-card__starring">
-                  <strong>
-                    Starring: {film.starring?.join(', ')} and other
-                  </strong>
-                </p>
-              </div>
+              <FilmTabs film={film}/>
             </div>
           </div>
         </div>
