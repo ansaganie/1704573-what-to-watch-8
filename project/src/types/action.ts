1226fa504daf6ca99d22650ee7122305d8536ac9
Ansagan Islamgali <islamgaliev@mail.ru>
2021-10-21 07:42:57 +0500

    TASK#8: Realizes show more button click handler

--- project/src/types/action.ts
+++ project/src/types/action.ts
@@ -1,7 +1,10 @@
-import { setGenre } from '../store/action';
+import { setGenre, setShownFilmsCount } from '../store/action';
 
 export enum ActionType {
-  SetGenre = 'main/set_genre'
+  SetGenre = 'main/set-genre',
+  SetShownFilmsCount = 'main/set-shown-films-count',
 }
 
-export type Actions = ReturnType<typeof setGenre>;
+export type Actions =
+  | ReturnType<typeof setGenre>
+  | ReturnType<typeof setShownFilmsCount>;
