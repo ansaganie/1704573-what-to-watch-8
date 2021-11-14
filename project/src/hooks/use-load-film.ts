cba3064f0305bbef755bed125d36e047ad9c836a
ansagan <islamgaliev@mail.ru>
2021-11-14 22:12:34 +0500

    TASK#16: Fixes useLoadFilm

--- project/src/hooks/use-load-film.ts
+++ project/src/hooks/use-load-film.ts
@@ -1,9 +1,8 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Film } from '../types/film';
 import { getFilms } from '../store/data/data-selectors';
-import { setFilmInFocus } from '../store/film/film-actions';
-import { getFilmInFocus, getIsFilmLoading } from '../store/film/film-selectors';
+import { getIsFilmLoading } from '../store/film/film-selectors';
 import { fetchFilm } from '../store/film/film-thunks';
 
 type UseFilmLoad = [ film: Film | null, isFilmLoading: boolean];
@@ -12,21 +11,23 @@ const useLoadFilm = (
   filmId: string,
 ): UseFilmLoad => {
   const dispatch = useDispatch();
-  const film = useSelector(getFilmInFocus);
+  const [ film, setFilm ] = useState<Film | null>(null);
   const isFilmLoading = useSelector(getIsFilmLoading);
   const films = useSelector(getFilms);
 
   useEffect(() => {
-    if (film?.id !== filmId) {
-      const result = films.find((item) => item.id === filmId);
+    const result = films.find((item) => item.id === filmId);
 
-      if (result) {
-        dispatch(setFilmInFocus(result));
-      } else {
-        dispatch(fetchFilm(filmId));
-      }
+    if (result) {
+      setFilm(result);
     }
-  }, [dispatch, film, filmId, films]);
+  }, [filmId, films]);
+
+  useEffect(() => {
+    if (!film) {
+      dispatch(fetchFilm(filmId));
+    }
+  }, [ film, filmId, dispatch ]);
 
   return [ film, isFilmLoading ];
 };
