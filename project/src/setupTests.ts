3910da65a31a24eb6528e4c88f0ae38ba3dc927f
ansagan <islamgaliev@mail.ru>
2021-11-18 08:11:26 +0500

    TASK#16: Creates tests for App routing

--- project/src/setupTests.ts
+++ project/src/setupTests.ts
@@ -8,12 +8,14 @@ import { Actions } from './store/store';
 const play = jest.fn().mockReturnValue(Promise.resolve());
 const load = jest.fn();
 const pause = jest.fn();
+const scrollTo = jest.fn();
 
 const unknownAction = (): Actions => ({ type: 'UNKNOWN_ACTION'} as unknown as Actions);
 
 window.HTMLMediaElement.prototype.load = load;
 window.HTMLMediaElement.prototype.play = play;
 window.HTMLMediaElement.prototype.pause = pause;
+window.scrollTo = scrollTo;
 
 Object.defineProperty(window.HTMLMediaElement.prototype, 'muted', {
   get: () => false,
